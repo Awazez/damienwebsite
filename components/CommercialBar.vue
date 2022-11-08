2364e18984b7753a62452bfef896574ae0c690d2
Martin AUBEUT <martin.aubeut@gmail.com>
2022-11-08 21:19:05 +0100

    initial commit

---
+++ components/CommercialBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <div>
+            
+        </div>
+
+    </div>
+    
+  </template>
+  
+  <style>
+  .container {
+    height: 44px;
+    max-width: 100%;
+    background-color: #FAF6F2;
+  }
+  </style>
