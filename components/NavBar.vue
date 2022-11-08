fa477e8516ae12a1733ad9999386caa36167387f
Martin AUBEUT <martin.aubeut@gmail.com>
2022-11-08 23:58:52 +0100

    doom commit

---
+++ components/NavBar.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="container">
+        <div class="container_left">
+            <div class="logo">
+            Damien Doussot 
+            <br>
+            Sexotherapeute
+            </div>
+        </div>
+       
+        
+
+    </div>
+    
+  </template>
+  
+  <style scoped>
+  
+.container {
+    height: 144px;
+    width: 100%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.container_left {
+    height: 144px;
+    width: 100%;
+    background-color: white;
+    display: flex;
+    align-items: center;
+    margin-left: 200px;
+
+}
+  
+
+
+  </style>
