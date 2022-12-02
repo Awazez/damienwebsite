223592059c9d0d362394ff875d6663242fa25226
Awazez <martin.aubeut@gmail.com>
2022-12-02 06:08:32 +0100

    add smiley

--- components/UsersSection.vue
+++ components/UsersSection.vue
@@ -1,6 +1,7 @@
 <template>
   <section class="bg-white">
-    <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+    <div class="gap-16 container items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
+      <h1>Avis des utilisateurs <span>🚀</span> </h1>
     </div>
 
 
@@ -16,4 +17,16 @@ export default {
 
 <style scoped>
 
+
+h1 {
+  width: 800px;
+  height: 450px;
+
+}
+
+.container {
+  width: 800px;
+  height: 450px;
+
+}
 </style>
