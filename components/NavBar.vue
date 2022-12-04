e642eb80ca729bc0d5eec89da497779ed155a820
Awazez <martin.aubeut@gmail.com>
2022-12-04 22:31:21 +0100

    change navbar titles

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -2,13 +2,13 @@
 <div class="bg-white">
         <nav class="container px-4 py-4 mx-auto md:flex md:justify-between md:items-center">
                 <div class="flex items-center justify-between">
-                <NuxtLink to="/" class="w-2/3 font-bold fonted"><img src=""></NuxtLink> 
+                <NuxtLink to="/" class="w-2/3 font-bold fonted"><img src=""> Damien Doussot</NuxtLink> 
                 </div>
         <!-- Mobile Menu open: "block", Menu closed: "hidden" -->
         <ul class="flex-col mt-8 space-y-4 md:flex md:space-y-0 md:flex-row md:items-center md:space-x-10 md:mt-0">
             <NuxtLink to="/"><li class="text-black hover:text-indigo-400">Accueil</li>           </NuxtLink>
-            <NuxtLink to="/PlanVélo"><li class="text-black hover:text-indigo-400">Le plan vélo</li> </NuxtLink>
-            <NuxtLink to="/InfosVélo"><li class="text-black hover:text-indigo-400">Infos vélo</li>   </NuxtLink>
+            <NuxtLink to="/PlanVélo"><li class="text-black hover:text-indigo-400">Compétences</li> </NuxtLink>
+            <NuxtLink to="/InfosVélo"><li class="text-black hover:text-indigo-400">Qui suis je?</li>   </NuxtLink>
             <NuxtLink to="/ContactPage"><li class="text-black hover:text-indigo-400">Contact</li>    </NuxtLink>
         </ul>
         </nav>
