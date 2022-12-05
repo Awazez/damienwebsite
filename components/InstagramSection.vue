bf1112985344b40b5662701bd2c60a208f15a669
Awazez <martin.aubeut@gmail.com>
2022-12-05 19:59:55 +0100

    Initialize InstagramSection component

---
+++ components/InstagramSection.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+
+    </div>
+
+
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+
+h1 {
+  margin-top: 105px;
+  font-size: 40px;
+  text-align: center;
+}
+
+
+</style>
