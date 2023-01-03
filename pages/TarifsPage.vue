d52bca7139a2325e026115e0b46a288274b70273
cherbourg@fub.fr <cherbourg@fub.fr>
2023-01-03 23:32:07 +0100

    Add tarifs page content

--- pages/TarifsPage.vue
+++ pages/TarifsPage.vue
@@ -2,8 +2,32 @@
     <div>
         <CommercialBar/>
     <NavBar/>
-    </div>
-    
+
+    <div class="">
+
+    <h1>Le cabinet & tarifs</h1>
+
+Séance individuelle (1h)
+Adultes, adolescents				65 €			Réservez une séance
+Étudiants, demandeurs d’emploi		50 € 			Réservez une séance
+
+Séance de respiration individuelle (1h30)	90 €			Réservez une séance
+
+Séance de couple (1h30)			90 €			Réservez une séance
+
+
+
+Les consultations ne sont pas remboursées par la sécurité sociale, cependant certaines assurances de complémentaire santé peuvent proposer une prise en charge.
+
+L’aspect financier ne doit pas être un obstacle, vous pouvez me contacter pour que nous trouvions ensemble des solutions.
+
+Possibilité de règlement par chèque, espèces ou carte bancaire.
+
+J’attire votre attention sur le fait qu’aucune des prestations proposées dans le cadre de mon accompagnement ne vous dispense d’une visite chez votre médecin. 
+Si vous suivez un traitement médical, il doit impérativement être poursuivi. 
+Les prestations proposées viennent en complément et peuvent faciliter l’action de vos traitements médicaux ou de votre accompagnement psychologique. Elles ne peuvent en aucun cas les remplacer.
+</div>
+</div>
 
 </template>
 
@@ -14,4 +38,12 @@ export default {
 
 </script>
 
-<style>
+<style scoped>
+
+h1 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+</style>
