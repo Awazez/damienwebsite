9997f906169e6c265f0f360e00460e463414470a
Awazez <martin.aubeut@gmail.com>
2023-01-12 19:28:24 +0100

    Add mise en page mentionlegal

--- pages/MentionLegal.vue
+++ pages/MentionLegal.vue
@@ -2,9 +2,77 @@
     <div>
       <CommercialBar/>
       <NavBar/>
+      <div class="center">
+      <br>
+      <h1>AVERTISSEMENT</h1>
+      <br>
+      <p>Le présent site est la propriété de Damien DOUSSOT.
+      Coût de connexion et de navigation internet variable en fonction de l’offre souscrite auprès de votre fournisseur d’accès à Internet ou opérateur mobile.</p>
+      <br>
+      <h1>CONTACT</h1>
+      <br>
+      <p>
+        Damien DOUSSOT
+        <br>
+        3 Rue Claude Chappe, 14000 Caen
+        <br>
+        Téléphone : 02 31 56 69 59
+        <br>
+        Adresse e-mail : damien.doussot@gmail.com
+      </p>
+      <br>
+      <h1>ÉDITEUR DU SITE INTERNET</h1> <br>
+      <p>
+        FREN WAR AGENCY 
+        <br>
+        mail : frenwaragency@gmail.com
+      </p>
+      <br>
+      <h1>HÉBERGEMENT</h1> 
+      <br>
+      Vercel
+      <br>
+      <br>
+      <h1>CRÉDITS</h1>
+      <br>
+      webdesign : ©FREN WAR AGENCY
+      <br>
+      <br>
+      <h1>OBJECTIF ET QUALITÉ DES CONTENUS/ RESPONSABILITÉ</h1>
+      <br>
+      <p>
+        Ce site a pour objectif d’informer le grand public. Damien DOUSSOT s’efforce de fournir une information de qualité et vérifiée, toutefois si une information semble inexacte ou contient une erreur typographique, vous pouvez le signaler à l’administrateur du site à l’adresse e-mail frenwar[at]gmail.com.
+        Damien DOUSSOT ne peut toutefois être tenue pour responsable de l’utilisation des contenus. Damien DOUSSOT apporte tous ses soins à la constitution et à la mise à jour des informations. En raison de la complexité du traitement des informations collectées et de la difficulté de contrôle par regroupement des sources, vous devez utiliser les résultats avec toute la prudence possible, Damien DOUSSOT ne pouvant être tenue pour responsable des dommages indirects dans une obligation juridique ou morale, perte d’un marché, préjudice commercial, perte de clientèle, trouble commercial quelconque, perte de bénéficies, perte d’image de marque, qui pourraient résulter de l’utilisation de ce site, des bases de données, de l’incapacité à utiliser celle-ci, de la nature ou de la complétude des informations contenues dans la base de données ou de l’utilisation des informations.
+        Le site vous propose des liens vers d’autres sites qui vous feront sortir du site www.damiendoussot.fr. Le contenu de ces sites n’engage pas la responsabilité de Damien DOUSSOT.
+        Nous ne garantissons pas que site soit exempt de défaut, d’erreur ou d’omission et qu’il corresponde exactement à l’usage auquel l’utilisateur le destine. Nous ne pouvons en aucun cas être tenus responsables des éventuels défauts, erreurs ou omissions existant dans ce site, et des éventuelles pertes financières ou autres dommages pouvant en résulter.
+        Damien DOUSSOT ne peut être tenue pour responsable d’éventuels dommages, directs ou indirects, découlant de votre accès ou utilisation de ce site. De plus, Damien DOUSSOT ne saurait être tenue pour responsable d’un dommage ou virus qui pourrait affecter votre téléphone portable ou tout matériel informatique, suite à une utilisation ou accès au site ou téléchargement provenant de ce site.
+      </p>
       <FooterBar/>
     </div>
+  </div>
   </template>
   <script>
   </script>
+
+<style scoped>
+
+p {
+  width: 50%;
+  height: 80%;
+  text-align: justify;
+  display : flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.center {
+  display : flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+
+}
+
+</style>
   
