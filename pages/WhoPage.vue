6fe0875e44c744c6301c0f129f03c2bf3653f395
cherbourg@fub.fr <cherbourg@fub.fr>
2023-03-02 20:27:42 +0100

    div centered

--- pages/WhoPage.vue
+++ pages/WhoPage.vue
@@ -3,45 +3,29 @@
         <CommercialBar/>
     <NavBar/>
     <section>
+        <h2>Qui suis je ?</h2>
         <div class="container">
+            
     <div class="container-left">
         <div>
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-black">Qui suis je ?</h2>
-                <img src="@/assets/img_damien.png" alt="office content 2">
+            
+                <img src="@/assets/Rectangle 18.png" alt="office content 2">
         </div>
     </div>
         <div class="container-right">
-            <p>
+            <p> <br><br><br><br>
                 Dès mon plus jeune âge, j’ai eu besoin de comprendre et de mettre du sens sur le fonctionnement de la vie et des comportements humains. 
 
-Suite à un parcours juridique dans de grandes entreprises, j’ai eu besoin de revenir à mes racines et retrouver du sens à ma vie. Je me suis alors intéressé au développement personnel, à la psychologie et ouvert à la spiritualité. 
+                <br><br> Suite à un parcours juridique dans de grandes entreprises, j’ai eu besoin de revenir à mes racines et retrouver du sens à ma vie. Je me suis alors intéressé au développement personnel, à la psychologie et ouvert à la spiritualité. <br><br>
 
 J’ai entrepris un travail sur moi pour comprendre ma structuration, mes blocages et mes enjeux personnels. J’ai ainsi ouvert une porte merveilleuse sur mon monde intérieur pour me réaliser et je continue de cheminer chaque jour en ce sens.
-
-Cela m'a permis de m'intéresser au fonctionnement de l’inconscient et aux outils pour y avoir accès. Je me suis alors formé à la psychothérapie transpersonnelle et aux états modifiés de conscience afin de toucher d’autres dimensions plus profondes de l’être humain.
-
-Passionné par le corps humain et son rapport à la psychologie, je me suis également formé à la sexothérapie afin de comprendre comment notre sexualité peut être le reflet de nos enjeux psychiques et permettre aux femmes et aux hommes d’aller connecter leur essence.
-
-Aujourd’hui, je me consacre à ce qui me fait vraiment vibrer, vous accompagner à aller à la rencontre de votre monde intérieur afin que vous puissiez vous réaliser.
-
-Déontologie
-Pour aborder un domaine aussi intime et personnel que la sexualité et la vie de couple, vous avez besoin d’avoir confiance en votre thérapeute.
-Le sexothérapeute est soumis au secret professionnel, ce qui signifie que ce qui se dit dans le cabinet reste dans le cabinet.
-Lors des entretiens, le thérapeute s’abstient de formuler tout jugement quant aux confidences qui lui sont faites et accueille la personne dans son authenticité.
-Pour les praticiens de la psychothérapie, le code de déontologie constitue un cadre de travail en énonçant plusieurs principes fondamentaux dans l’exercice de leur pratique.
-Il se fonde sur 4 piliers:
-	•	Respecter et favoriser l’autonomie de la personne qui consulte.
-	•	Respecter la confidentialité des propos échangés.
-	•	Développer les compétences du praticien, grâce à la formation initiale, en suivant une thérapie personnelle, en étant suivi dans sa pratique professionnelle (supervision) et en se formant en continu.
-	•	Travailler en réseau afin d’orienter les personnes qui pourraient avoir besoin d’un suivi médical.​
-
-
 
  
             </p>
             </div>
     
 </div>
+<div class="container-text"></div>
 </section>
     </div>
 
@@ -58,23 +42,37 @@ export default {
 
 @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
 
+
+h2 {
+    display:flex;
+    align-items: center;
+    justify-content: center;
+}
+
 .container {
     display: flex;
     align-items: center;
-    width: 100%;
+    justify-content: center;
+    width: auto;
+    margin: auto;
 }
+
 
 .container-left {
     font-size: 24px;
-    width: 100%;
-    margin-left: 200px;
+    width: 600px;
+    height: 200px;
+    margin-left: 100px;
 }
 
 .container-right{
-    position: relative;
     font-size: 24px;
-    width: 100%;
     text-align: justify;
+
+}
+
+.container-text {
+
 }
 
 
