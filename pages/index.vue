80e3577cc3978eb1b526177276f8863a78c0251c
cherbourg@fub.fr <cherbourg@fub.fr>
2023-03-06 17:46:33 +0100

    refacto index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -12,19 +12,7 @@
   </div>
 </template>
 <script>
-import ThreePoint from '~/components/ThreePoint.vue';
 
 export default {
-    name: "IndexPage",
-    components: { ThreePoint },
-    data: () => ({
-      slides: [
-        "https://picsum.photos/id/1032/900/400",
-        "https://picsum.photos/id/1033/900/400",
-        "https://picsum.photos/id/1034/900/400",
-        "https://picsum.photos/id/1035/900/400",
-        "https://picsum.photos/id/1036/900/400",
-      ]
-    })
 }
 </script>
