68ea9faaa5605e575e40b65099ff3d6d53e5ed26
cherbourg@fub.fr <cherbourg@fub.fr>
2023-03-06 18:04:51 +0100

    add presentation on whopage !

--- pages/WhoPage.vue
+++ pages/WhoPage.vue
@@ -19,13 +19,34 @@
                 <br><br> Suite à un parcours juridique dans de grandes entreprises, j’ai eu besoin de revenir à mes racines et retrouver du sens à ma vie. Je me suis alors intéressé au développement personnel, à la psychologie et ouvert à la spiritualité. <br><br>
 
 J’ai entrepris un travail sur moi pour comprendre ma structuration, mes blocages et mes enjeux personnels. J’ai ainsi ouvert une porte merveilleuse sur mon monde intérieur pour me réaliser et je continue de cheminer chaque jour en ce sens.
-
- 
             </p>
             </div>
-    
+            
 </div>
-<div class="container-text"></div>
+<br><br>
+<br><br>
+<div class="container-text">
+    <div class="container-title">
+        Passionné par le corps humain et son rapport à la psychologie, je me suis également formé à la sexothérapie afin de comprendre comment notre sexualité peut être le reflet de nos enjeux psychiques et permettre aux femmes et aux hommes d’aller connecter leur essence.
+
+Aujourd’hui, je me consacre à ce qui me fait vraiment vibrer, vous accompagner à aller à la rencontre de votre monde intérieur afin que vous puissiez vous réaliser.
+    </div>
+</div>
+
+<div class="container-perso">
+Déontologie
+Pour aborder un domaine aussi intime et personnel que la sexualité et la vie de couple, vous avez besoin d’avoir confiance en votre thérapeute.
+Le sexothérapeute est soumis au secret professionnel, ce qui signifie que ce qui se dit dans le cabinet reste dans le cabinet.
+Lors des entretiens, le thérapeute s’abstient de formuler tout jugement quant aux confidences qui lui sont faites et accueille la personne dans son authenticité.
+Pour les praticiens de la psychothérapie, le code de déontologie constitue un cadre de travail en énonçant plusieurs principes fondamentaux dans l’exercice de leur pratique.
+Il se fonde sur 4 piliers:
+Respecter et favoriser l’autonomie de la personne qui consulte.
+Respecter la confidentialité des propos échangés.
+Développer les compétences du praticien, grâce à la formation initiale, en suivant une thérapie personnelle, en étant suivi dans sa pratique professionnelle (supervision) et en se formant en continu.
+Travailler en réseau afin d’orienter les personnes qui pourraient avoir besoin d’un suivi médical.​
+</div>
+
+
 </section>
     </div>
 
@@ -47,6 +68,8 @@ h2 {
     display:flex;
     align-items: center;
     justify-content: center;
+    font-size: 40px;
+    font-weight: 700;
 }
 
 .container {
@@ -72,9 +95,31 @@ h2 {
 }
 
 .container-text {
-
+    padding-top: 50px;
+    padding-bottom:  50px;
+    width: 100%;
+    height: 600px;
+    font-size: 40px;
+    border: none;
+    background-color: #111827;
+    color: white;
+    text-align: center;
 }
 
+.container-title {
+    width: 1000px;
+    margin: auto;
+}
+
+.container-perso {
+    width: 1000px;
+    margin: auto;
+    height: 1000px;
+    background-color: white;
+    font-size: 24px;
+    text-align: center;
+
+}
 
 
 </style>
